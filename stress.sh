81918465141fc0b77bde9fd1380ff9320ade88ef
mktx-code <mktx-code@users.noreply.github.com>
2015-06-22 07:17:51 +0000

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -19,23 +19,23 @@ echo ""; echo -en "[+] FIRST ADDRESS: "$addr"\n[+] FIRST PRIVKEY: "$priv""; echo
 # Initial Funding #
 echo -e "\nYOU SHOULD FUND THIS ADDRESS NOW.\nAFTER IT IS FUNDED PRESS ENTER."; read
 sum=$($(echo -n "$pybtc") unspent "$addr" | ./pybtctool -j multiaccess value | ./pybtctool -j sum)
-while [[ "$sum" -lt "1" ]]; do echo ""; echo "NO SATOSHIS IN THIS ADDRESS YET."; echo ""
+while [[ "$sum" -lt "1" ]]; do echo ""; echo "NO BITS IN THIS ADDRESS YET."; echo ""
 echo -e "YOU SHOULD FUND THIS ADDRESS NOW.\nAFTER IT IS FUNDED PRESS ENTER."; read
 sum=$($(echo -n "$pybtc") unspent "$addr" | ./pybtctool -j multiaccess value | ./pybtctool -j sum); done
-echo ""; read -p "Set fee in satoshis (suggested 10000): " fee; pos=$(($sum / $fee))
-echo ""; echo "[+] FUNDED: "$addr""; echo "[+] AVAILABLE SATOSHIS: "$sum""; echo "[+] TRANSACTIONS POSSIBLE: "$pos""
+echo ""; read -p "Set fee in bits (10000 = .0001 BTC): " fee; pos=$(($sum / $fee))
+echo ""; echo "[+] FUNDED: "$addr""; echo "[+] AVAILABLE BITS: "$sum""; echo "[+] TRANSACTIONS POSSIBLE: "$pos""
 echo ""; read -p "Set number of seconds between transactions: " sec; while [[ -z "$sec" ]]; do echo ""
 echo "YOU MUST ENTER A VALID NUMBER!"; echo ""; read -p "Set number of seconds between transactions: " sec; done
 sec=$(($sec - 3)); totx=$(($pos * $sec)); echo ""; echo "[+] TOTAL TIME TO COMPLETE: "$totx" SECONDS"; echo ""
-echo "ONCE YOU START THE SCRIPT IT WILL CONTINUALLY GENERATE NEW ADDRESSES USING THE SEED CREATED IN THE BEGINNING. SENDING ALL AVAILABLE SATOSHIS TO THE NEXT DETERMINISTIC ADDRESS SPENDING ONLY THE "$fee" SATOSHI FEE THAT YOU SET ON EACH TRANSACTION."; echo ""
+echo "ONCE YOU START THE SCRIPT IT WILL CONTINUALLY GENERATE NEW ADDRESSES USING THE SEED CREATED IN THE BEGINNING. SENDING ALL AVAILABLE BITS TO THE NEXT DETERMINISTIC ADDRESS SPENDING ONLY THE "$fee" BIT FEE THAT YOU SET ON EACH TRANSACTION."; echo ""
 echo "IN THE CASE THAT THERE IS A PROBLEM, YOUR SEED AND ALL ADDRESSES/PRIVATE KEYS WILL BE STORED IN "$PWD"/stest.bak."
 echo ""; read -p "PRESS ENTER TO BEGIN STRESSING THE NETWORK."
 # Begin Stressing #
 while [[ "$sum" -gt "$fee" ]]; do ntot=$(($totx - $sec)); echo ""; echo "[+] STRESSING FINISHED IN APROX. "$ntot" SECONDS..."
 i=$(cat stest.bak | grep -c "Privkey"); npriv=$(./pybtctool bip32_ckd "$s" "$i" | ./pybtctool -s bip32_extract_key)
 naddr=$(./pybtctool privtoaddr "$npriv"); echo -en "\nAddress "$i": "$naddr"\nPrivkey "$i": "$npriv"\n" >> stest.bak
-amt=$(expr $sum - $fee); echo "[+] SENDING "$amt" SATOSHIS TO "$naddr...""
+amt=$(expr $sum - $fee); echo "[+] SENDING "$amt" BITS TO "$naddr...""
 stx=$($(echo -n "$pybtc") unspent "$addr" | ./pybtctool -j select "$sum" | ./pybtctool -j mktx "$naddr":"$amt" | ./pybtctool -s sign 0 "$priv"); $(echo -n "$pybtc") pushtx "$stx"; sleep "$sec"; priv="$npriv"; addr="$naddr"
 sum=$($(echo -n "$pybtc") unspent "$addr" | ./pybtctool -j multiaccess value | ./pybtctool -j sum)
 pos=$(($sum / $fee)); totx=$(($pos * $sec)); done
-echo ""; echo "[+] OUT OF SATOSHIS!"; echo ""; read -p "PRESS ENTER TO QUIT"; exit 0
+echo ""; echo "[+] OUT OF BITS!"; echo ""; read -p "PRESS ENTER TO QUIT"; exit 0
